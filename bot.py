17425c981f2012291e63093c8a2a113f51d3fdd9
Luiz <luizgomes749@gmail.com>
2019-05-11 11:14:57 -0300

    Update

--- bot.py
+++ bot.py
@@ -20,6 +20,7 @@ def clear():
 # Loading config file...
 with open("./config.json", "r", encoding="utf-8") as config:
     j = json.load(config)
+    config.Close()
 
 # Defining the token (got from .json file)
 token = j["config"]["token"]
@@ -27,15 +28,12 @@ token = j["config"]["token"]
 # Initializing the client
 client = discord.Client()
 
-
-"""
-╔╗║╟╢
-"""
-
 # Doing some work at startup...
 @client.event
 async def on_ready():
     clear()
+    
+    # Formatting the clients name to fit into the box        
     name = str(client.user)
     name_length = len(name)
     act_char = 0
@@ -51,15 +49,23 @@ async def on_ready():
     else:
         name_ = name
     
+    # The box    
     print(" ╔══════════════════════════════════════╗")
     print(" ║ => We're ready to go!                ║")
     print(" ╟──────────────────────────────────────╢")
     print(" ║{: ^38}║".format(name_))
     print(" ╟──────────────────────────────────────╢")
     print(" ╚══════════════════════════════════════╝")
+    
+    # Console screen title (just for, idk, make it better, i guess)
     system('TITLE Expertise Bot Core :: Rewrite v0.0.1 :: Online with [ {} ]'.format(name))
-    
-    
+
+
+# Reading all extensions added in config.json
+extensions = j["extensions"]
+
+
+
 
 # Running the client
 client.run(token)
