548d07502e01b558321158a85766b861a2014df8
Luiz <luizgomes749@gmail.com>
2019-05-10 23:58:43 -0300

    Reading json configuration file, running the bot with the token got from it.

--- bot.py
+++ bot.py
@@ -1,13 +1,24 @@
 import discord
+import asyncio
 import json
 
-with open("./config.json", "r", encoding="utf-8") as config:
-    j = config.dump()
-    config.Close()
-    
-print (j["config"]["token"])
 
+
+# Loading config file...
+with open("./config.json", "r", encoding="utf-8") as config:
+    j = json.load(config)
+
+# Defining the token (got from .json file)
+token = j["config"]["token"]
+
+# Initializing the client
 client = discord.Client()
 
+# Doing some work at startup...
+@client.event
+async def on_ready():
+    print (" => Bot online!")
 
 
+# Running the client
+client.run(token)
