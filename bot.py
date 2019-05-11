ccbfa11a981b2168f8800db786a22600f8c407b2
Luiz <luizgomes749@gmail.com>
2019-05-10 23:51:06 -0300

    "Added json config file parsing"

--- bot.py
+++ bot.py
@@ -2,7 +2,10 @@ import discord
 import json
 
 with open("./config.json", "r", encoding="utf-8") as config:
-    j = config.
+    j = config.dump()
+    config.Close()
+    
+print (j["config"]["token"])
 
 client = discord.Client()
 
