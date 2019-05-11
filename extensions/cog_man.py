38cca52f95730516950720a552b6b22b38d96b80
Luiz <luizgomes749@gmail.com>
2019-05-11 13:31:26 -0300

    Added cog_man(for managing all cogs)

---
+++ extensions/cog_man.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+
+class Cogs(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command(name="reload", pass_context=True)
+    @commands.is_owner
+    async def reload(self, ctx):
+        ctx.send("Reloading...")
+        
+
+
+
+def setup(client):
+    client.add_cog(Cogs(client))
