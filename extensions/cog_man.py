9893300f331123dc42954356d3342ae37c4e5891
Luiz A F Gomes <luizgomes749@gmail.com>
2020-02-23 22:57:08 -0300

    Cog man now loads all cogs

--- extensions/cog_man.py
+++ extensions/cog_man.py
@@ -1,9 +1,10 @@
 import json
 
 # Imports
+import discord
 from discord.ext import commands
 
-class Cogs(commands.Cog):
+class CogMan(commands.Cog):
     def __init__(self, client):
         self.client = client
         
@@ -21,10 +22,9 @@ class Cogs(commands.Cog):
                 print(f" ~> Cannot load cog due to [{e}]")
 
     @commands.command(name="reload", pass_context=True)
-    @commands.is_owner
     async def reload(self, ctx):
         ctx.send("Reloading...")
 
 
 def setup(client):
-    client.add_cog(Cogs(client))
+    client.add_cog(CogMan(client))
