b684352ea8c906caec64dae01c20c94ab29da110
Luiz A F Gomes <luizgomes749@gmail.com>
2020-02-23 23:37:56 -0300

    Added plugin reloading stuff

--- extensions/ext_mgr.py
+++ extensions/ext_mgr.py
@@ -21,6 +21,7 @@ class CogMan(commands.Cog, name="Extension manager for ExpertiseBot"):
             except Exception as e:
                 print(f" ~> Cannot load cog due to [{e}]")
 
+
     @commands.command(name="reload", pass_context=True)
     async def reload(self, ctx):
         await ctx.send("Reloading extensions!")
@@ -28,13 +29,18 @@ class CogMan(commands.Cog, name="Extension manager for ExpertiseBot"):
         self.extensions = self.j["extensions"]
         for extension in self.extensions:
             try:
-                client.cog_unload(extension)
-                print(f" ~> Extension [{extension}] unloaded with success!")
-                client.load_extension(extension)
-                print(f" ~> Extension [{extension}] loaded successfully!")
+                self.client.reload_extension(extension)
+                print(f" ~> Extension {extension} reloaded successfully!")
             except Exception as e:
-                print(f" ~> Cannot load cog due to [{e}]")
+                print(f" ~> Cannot reload cog due to [{e}]")
+                await ctx.send("Couldn't reload all extensions!")
 
+        await ctx.send("All extensions reloaded successfully!")
+
+    @commands.command(name="install", pass_context=True)
+    async def install(self, ctx, url):
+        await ctx.send(f"Instalar {url}")
+        
 
 def setup(client):
     client.add_cog(CogMan(client))
