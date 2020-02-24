17d8d274bd09a2fb3b7cab35685941afac9ef8c9
Luiz A F Gomes <luizgomes749@gmail.com>
2020-02-24 00:05:31 -0300

    Added hasher command

--- extensions/ext_mgr.py
+++ extensions/ext_mgr.py
@@ -1,4 +1,6 @@
 import json
+import time
+import hashlib
 
 # Imports
 import discord
@@ -24,23 +26,30 @@ class CogMan(commands.Cog, name="Extension manager for ExpertiseBot"):
 
     @commands.command(name="reload", pass_context=True)
     async def reload(self, ctx):
-        await ctx.send("Reloading extensions!")
-        # Reading all extensions added in config.json
-        self.extensions = self.j["extensions"]
-        for extension in self.extensions:
-            try:
-                self.client.reload_extension(extension)
-                print(f" ~> Extension {extension} reloaded successfully!")
-            except Exception as e:
-                print(f" ~> Cannot reload cog due to [{e}]")
-                await ctx.send("Couldn't reload all extensions!")
+        async with ctx.typing():
+            await ctx.send("Reloading extensions!")
+            # Reading all extensions added in config.json
+            self.extensions = self.j["extensions"]
+            for extension in self.extensions:
+                try:
+                    self.client.reload_extension(extension)
+                    print(f" ~> Extension {extension} reloaded successfully!")
 
-        await ctx.send("All extensions reloaded successfully!")
+                except Exception as e:
+                    print(f" ~> Cannot reload cog due to [{e}]")
+                    await ctx.send("Couldn't reload all extensions!")
+
+            await ctx.send("All extensions reloaded successfully!")
 
     @commands.command(name="install", pass_context=True)
     async def install(self, ctx, url):
         await ctx.send(f"Instalar {url}")
-        
+    
+    @commands.command(name="ghash", pass_context=True)
+    async def ghash(self, ctx, name):
+        fname = hashlib.sha256(name.encode('utf-8')).hexdigest()
+        await ctx.send(f"Hi! To make your extension a valid extension, please,\nplace this value in the hash section inside your `meta.json`")
+        await ctx.send(f"```{fname}```")
 
 def setup(client):
     client.add_cog(CogMan(client))
