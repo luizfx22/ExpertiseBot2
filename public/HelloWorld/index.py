aa4fd90120cb068c97a8b37fdb501ee2a14098cb
Luiz <luizgomes749@gmail.com>
2020-03-06 19:44:10 -0300

    Added stuff

---
+++ public/HelloWorld/index.py
@@ -0,0 +1,14 @@
+'''
+Main file for the cog
+'''
+
+# Imports
+import discord
+from discord.ext import commands
+
+class HelloWorld(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+def setup(bot):
+    bot.add_cog(HelloWorld(bot))
