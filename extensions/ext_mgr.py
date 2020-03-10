f6617319b6e777a47ebe6c4bbadf4c1a1d0ddcf4
Luiz <luizgomes749@gmail.com>
2020-03-10 20:07:49 -0300

    Created plugins index file

--- extensions/ext_mgr.py
+++ extensions/ext_mgr.py
@@ -1,5 +1,7 @@
 import json
 import hashlib
+import sqlite3
+import os
 
 # Imports
 from discord.ext import commands
@@ -8,26 +10,58 @@ class CogMan(commands.Cog, name="Extension manager for ExpertiseBot"):
     def __init__(self, client):
         self.client = client
         
+        # Create or load SQLite3 database and cursor
+        try:
+            connection = sqlite3.connect("./extensions/plugins.db")
+        except Exception:
+            with open('./extensions/plugins.db', 'w') as dbFile:
+                dbFile.close()
+            connection = sqlite3.connect("./extensions/plugins.db")
+        
+        cursor = connection.cursor()
+
+        # Creating table if not exists
+        query = """
+            CREATE TABLE IF NOT EXISTS plugins (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                PluginName TEXT NOT NULL,
+                PluginMain TEXT NOT NULL,
+                PluginDirectory TEXT NOT NULL
+            )
+        """
+        cursor.execute(query)
+        connection.commit()
+
         # Loading config file...
         with open("./config.json", "r", encoding="utf-8") as config:
-            self.j = json.load(config)
+            self.configFile = json.load(config)
 
         # Reading all extensions added in config.json
-        self.extensions = self.j["extensions"]
+        self.extensions = self.configFile["extensions"]
         for extension in self.extensions:
             try:
                 client.load_extension(extension)
                 print(f" ~> Extension [{extension}] loaded with success!")
             except Exception as e:
                 print(f" ~> Cannot load cog due to [{e}]")
+        
+        print(" -> Indexing plugins...")
 
+        # Start indexing all installed plugins
+        plugins = cursor.execute("SELECT * FROM plugins")
+        for plugin in plugins:
+            print(plugin)
+        else:
+            print(" - Found nothing! Skipping...")
+            cursor.execute("INSERT INTO plugins (PluginName, PluginMain, PluginDirectory) VALUES ('Hello World', '/public/HelloWorld/main.json', '/public/HelloWorld/')")
+            connection.commit()        
 
     @commands.command(name="reload", pass_context=True)
     async def reload(self, ctx):
         async with ctx.typing():
             await ctx.send("Reloading extensions!")
             # Reading all extensions added in config.json
-            self.extensions = self.j["extensions"]
+            self.extensions = self.configFile["extensions"]
             for extension in self.extensions:
                 try:
                     self.client.reload_extension(extension)
@@ -41,8 +75,7 @@ class CogMan(commands.Cog, name="Extension manager for ExpertiseBot"):
 
     @commands.command(name="install", pass_context=True)
     async def install(self, ctx, options, name):
-        valid_opts = []
-        await ctx.send(f"Instalar {url}")
+        pass
     
     @commands.command(name="ghash", pass_context=True)
     async def ghash(self, ctx, name):
