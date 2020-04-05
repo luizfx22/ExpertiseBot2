2e8da09ad8d2ba5b64c9945b2c3396081acb7201
Luiz A F Gomes <luizgomes749@gmail.com>
2020-04-05 20:59:20 -0300

    Removed usermade cogs

--- extensions/ext_mgr.py
+++ extensions/ext_mgr.py
@@ -1,13 +1,9 @@
-import json
 import hashlib
 import sqlite3
 from pathlib import Path
 
 # Imports
 from discord.ext import commands
-
-def validatePlugin():
-    pass
 
 class CogMan(commands.Cog, name="Extension manager for ExpertiseBot"):
     def __init__(self, client):
@@ -26,9 +22,6 @@ class CogMan(commands.Cog, name="Extension manager for ExpertiseBot"):
                 print(f" ~> Extension [{extension}] loaded with success!")
             except Exception as e:
                 print(f" ~> Cannot load cog due to [{e}]")
-        
-        print(" -> Indexing plugins...")
-    
 
     @commands.command(name="reload", pass_context=True)
     async def reload(self, ctx):
@@ -46,16 +39,6 @@ class CogMan(commands.Cog, name="Extension manager for ExpertiseBot"):
                     await ctx.send("Couldn't reload all extensions!")
 
             await ctx.send("All extensions reloaded successfully!")
-
-    @commands.command(name="install", pass_context=True)
-    async def install(self, ctx, options, name):
-        pass
-    
-    @commands.command(name="ghash", pass_context=True)
-    async def ghash(self, ctx, name):
-        fname = hashlib.sha256(name.encode('utf-8')).hexdigest()
-        await ctx.send(f"Hi! To make your extension a valid extension, please,\nplace this value in the hash section inside your `meta.json`")
-        await ctx.send(f"```{fname}```")
 
 def setup(client):
     client.add_cog(CogMan(client))
