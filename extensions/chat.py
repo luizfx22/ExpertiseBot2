f11b679682c503277420c0bb642665903ea7768a
Luiz <luizgomes749@gmail.com>
2020-09-13 18:28:48 -0300

    Adding channels on creation and server sync with db

--- extensions/chat.py
+++ extensions/chat.py
@@ -1,19 +1,20 @@
 
 import mysql.connector
 from mysql.connector import errorcode
-from discord import Embed, Colour, Guild
+from discord import Embed, Colour, Guild, TextChannel
 from discord.ext import commands
 import re
 import json
 import random
 
+# Loading config file...
+with open("./config.json", "r", encoding="utf-8") as config:
+    configFile = json.load(config)
+    config.close()
+
 class ChatControl(commands.Cog, name="Chat management commands"):
     def __init__(self, client):
         self.client = client
-        
-        # Loading config file...
-        with open("./config.json", "r", encoding="utf-8") as config:
-            configFile = json.load(config)
 
         print(f' >> Connecting to database with user {configFile["db-settings"]["user"]}!')
 
@@ -38,31 +39,102 @@ class ChatControl(commands.Cog, name="Chat management commands"):
         
         self.cursor = self.connection.cursor()
     
+    # External functions
     async def setup_backup(self, ctx):
         message = ctx.message
         guild = ctx.guild
-
         guild_id = guild.id
+        guild_text_channels = guild.text_channels
 
-        sql = """INSERT IGNORE INTO sec_guilds (`id`, `name`) VALUES (%s, %s);"""
+        # Starting SQL stuff
+        sql_guilds = """INSERT IGNORE INTO sec_guilds (`id`, `name`) VALUES (%s, %s);"""
         
-        embed = Embed(title="ExpertiseBot Channel Backup Utility", description="Using ExpertiseBot to create a backup of every message from any text channel available in guild.")
-        embed.color = 0x5CA7AD
-        embed.add_field(name="How to:", value="To create a backup you must set the channels to be watched and backup every message!")
-        
-        guild_text_channels = get
-
-        await ctx.send(embed=embed)
-
-        self.cursor.execute(sql, (guild.id, guild.name))
-
+        # Commiting changes to DB
+        self.cursor.execute(sql_guilds, (guild.id, guild.name))
         self.connection.commit()
 
-        await ctx.send(message.id)
+        server_inserted = self.cursor.rowcount
+
+        if server_inserted < 1:
+            await ctx.send("Server already exists in database!")
+            
+        channels = []
+
+        sql_channels = """
+            INSERT IGNORE INTO sec_channels (
+                `id`,
+                `guild_id`,
+                `name`,
+                `fl_backup`,
+                `fl_log`,
+                `fl_active`
+            ) VALUES (
+                %s,
+                %s,
+                %s,
+                %s,
+                %s,
+                %s
+            );
+        """
+
+        for channel in guild_text_channels:
+            cnnl = (channel.id, guild_id, channel.name, 0, 0, 1)
+            channels.append(cnnl)
         
+        self.cursor.executemany(sql_channels, channels)
+        self.connection.commit()
+
+        channels_inserted = self.cursor.rowcount
+
+        channels.clear()
+        
+        # UI
+        embed = Embed(title="ExpertiseBot Channel Backup Utility", description="Using ExpertiseBot to create a backup of every message from any text channel available in this server (except private channels).")
+        embed.color = 0x5CA7AD
+        embed.add_field(name="How to:", value="To create a backup you must set the channels to be watched and backup every message!", inline=False)
+        
+        # Selecting all channels in database
+        sql_list_channels = """
+            SELECT
+                `id`,
+                `guild_id`,
+                `name`,
+                `fl_backup`,
+                `fl_log`,
+                `fl_active`
+            FROM
+                sec_channels
+            WHERE 
+                `guild_id` = %s
+            ORDER BY `name` ASC;
+        """
+        self.cursor.execute(sql_list_channels, (guild_id,))
+
+        found_channels = self.cursor.fetchall()
+        
+        # Adding results to Embed
+        for channel in found_channels:
+            backing_up = False
+            if channel[3] == 1:
+                backing_up = True
+
+            embed.add_field(name=f"{channel[2]}:", value=f"Backup enabled: {'✔️' if backing_up else '❌'}", inline=False)
+
+        # Send embed
+        await ctx.send(embed=embed)
+
+        if server_inserted > 0:
+            success = Embed(title="ExpertiseBot Channel Backup Utility", description="Success! Server registered successfully!")
+            success.color = 0x0E8A00
+            await ctx.send(embed=success)
 
         return True
     
+    async def setup_set_log(self, ctx, channel):
+        print(channel)
+
+    # Commands
     @commands.command(pass_context = True)
     async def clear(self, ctx, amount = 10):
         if amount < 1:
@@ -104,12 +176,68 @@ class ChatControl(commands.Cog, name="Chat management commands"):
         await ctx.send(embed=embed)
     
     @commands.command(pass_context=True)
-    async def backup(self, ctx, command):
+    async def backup(self, ctx, command, *args):
         commands = {
-            "setup": await self.setup_backup(ctx)
+            "setup": self.setup_backup,
+            "setlog": self.setup_set_log
         }
 
-        return commands[command]
+        return await commands[command](ctx, args)
+    
+    # Events
+    @commands.Cog.listener()
+    async def on_guild_channel_create(self, channel):
+        insert_channel_sql = """
+            INSERT IGNORE INTO sec_channels (
+                `id`,
+                `guild_id`,
+                `name`,
+                `fl_backup`,
+                `fl_log`,
+                `fl_active`
+            ) VALUES (
+                %s,
+                %s,
+                %s,
+                %s,
+                %s,
+                %s
+            );
+        """
+
+        self.cursor.execute(insert_channel_sql, (channel.id, channel.guild.id, channel.name, 0, 0, 1))
+        self.connection.commit()
+
+        channel_inserted = self.cursor.rowcount
+        
+        if channel_inserted > 0:
+            success = Embed(title="ExpertiseBot Channel Backup Utility", description="Success! Channel successfully registered!")
+            success.color = 0x0E8A00
+            success.add_field(name="To add it to backup list",
+                              value=f"run `{configFile['config']['default_prefix']}backup add {channel.mention}`")
+
+            await channel.send(embed=success)
+        else:
+            error = Embed(title="ExpertiseBot Channel Backup Utility", description="Error! Couldn't register channel!")
+            error.color = 0xFF0000
+            error.add_field(name="Maybe this server is not registered yet... To do that",
+                              value=f"run `{configFile['config']['default_prefix']}backup setup`")
+
+            await channel.send(embed=error)
+
+        return True
+
+    @commands.Cog.listener()
+    async def on_guild_channel_delete(self, channel):
+        # remove_channel_sql = """
+        #     DELETE FROM sec_channels WHERE `id` = %s;
+        # """
+
+        # self.cursor.execute(remove_channel_sql, (channel.id,))
+        # self.connection.commit()
+
+        print(channel.guild.owner)
+        
 
 def setup(client):
     client.add_cog(ChatControl(client))
