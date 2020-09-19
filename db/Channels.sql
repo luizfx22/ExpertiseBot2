c0a499a7b7b2762959da85a7a51b33752b3f14f1
Luiz <luizgomes749@gmail.com>
2020-09-19 18:51:16 -0300

    Added deleted_at

--- db/Channels.sql
+++ db/Channels.sql
@@ -1,14 +1,15 @@
 create table sec_channels
 (
-    id        bigint               not null,
-    guild_id  bigint               not null,
-    `name`      varchar(255)         not null,
-    fl_backup tinyint(1) default 0 not null,
-    fl_log    tinyint(1) default 0 null,
-    fl_active tinyint(1) default 1 null,
+    id         bigint               not null,
+    guild_id   bigint               not null,
+    name       varchar(255)         not null,
+    fl_backup  tinyint(1) default 0 not null,
+    fl_log     tinyint(1) default 0 null,
+    fl_active  tinyint(1) default 1 null,
+    fl_deleted tinyint(1) default 0 not null,
+    deleted_at datetime             null,
     constraint sec_channels_id_uindex
         unique (id),
     constraint sec_channels_sec_guilds_id_fk
         foreign key (guild_id) references sec_guilds (id)
 );
-
